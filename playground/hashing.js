68913e5898273b6b860d36dc51e28eedf232fc25
Spiral Wood <musictoon101@gmail.com>
2017-05-09 03:02:46 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abd!';
 
-var token = jwt.sign(data, 'abc123')
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//       console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, 'abc123');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$TQRweOHOyZ4cVsvOO65nYuLAY4FHiDehHK.IapRRhDjZFvk2ldhJK';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, 'abc123')
+// console.log(token);
+//
+// var decoded = jwt.verify(token, 'abc123');
+// console.log('decoded', decoded);
 
 // var message ='I am a number not a name';
 // var hash = SHA256(message).toString();
